05cb3331bb74dc2492ba6283fc5c8ab1b4194440
DavidUllauri <ullauridavid@gmail.com>
2022-04-19 23:43:58 -0400

    Added stbi and glad

--- Texturetest/main.cpp
+++ Texturetest/main.cpp
@@ -1,3 +1,35 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb_image.h>
+
+//#include <learnopengl/shader_s.h>
 
 #include <iostream>
+
+int main()
+{
+	if (!glfwInit())
+		std::cout << "ERROR: Initialization failed" << std::endl;
+
+	// Create Window /////////////////////////////////////////
+	GLFWwindow* window = glfwCreateWindow(800, 600, "game window", NULL, NULL);
+
+	if (!window) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Shader //////////////////////////////////////////////
+
+
+	return 0;
+}
